c99f4ef8782a7ac0f349d2b328c7f27ac8f24165
Alexander Boyd <874710@hboe.org>
2017-11-21 13:35:34 -0500

    fixed gravity and added background image (broken)

--- platformer.js
+++ platformer.js
@@ -2,16 +2,51 @@
 let bob;
 let jim;
 let score;
+var bg;
 
+
+ function setup(){
+  bg = loadImage ("https://ak6.picdn.net/shutterstock/videos/14626936/thumb/4.jpg")
+  createCanvas(852, 480);
+  score = 0;
+  let x = 100;
+  let y = 100;
+  let width = 200;
+  bob = new Platform(x,y,width);
+  jim = new Hero();
+ }
+  //for (let i = 0; i < 1; i++)	  {
+	//let x = 5;
+	//let y = 10;
+	//}
+
+function fallen(){
+	if(jim.y > height){
+		jim.y = 0;
+		score--;
+	}
+}
+	
+function draw(){
+	background(bg);
+	jim.show();
+	jim.move();
+	bob.show();
+	bob.show();
+	fallen();
+	textSize(22);
+	text("Score: "+score, 10, 90);
+	
+}
 class Platform {
-	constructor(x, y){
+	constructor(x, y, width){
 		this.x = x;
 		this.y = y;
-		this.width = w;
+		this.width = width;
 		this.height = 20;
 	}
 	contains(givenX, givenY){
-		if (givenX > this.x && this.x < this.x + this.width && givenY > this.y && this.y < this.y + this.width){
+		if (givenX > this.x && givenX < this.x + this.width && givenY > this.y && givenY < this.y + this.height){
 			return true;
 		}
 		else{
@@ -22,49 +57,14 @@ class Platform {
 	show(){
 		rect(this.x, this.y, this.width, this.height);
 	}
-	}
-	
- function setup(){
-  createCanvas(600, 400);
-  score = 0;
-  let x = 100;
-  let y = 100;
-  let width = 200;
-  bob = new Platform(x,y,width);
-  jim = new Hero();
- }
-  
-  //for (let i = 0; i < 1; i++)	  {
-	//let x = 5;
-	//let y = 10;
-	//}
-
-function fallen(){
-	if(jim.y < height){
-		jim.y = 0;
-		score--;
-	}
-}
-	
-function draw(){
-	background("gray");
-	jim.showHero();
-	bob.showPlat();
-	Text("Score: "+score, 20 90);
-	
 }
 
-function drawScore(){
-	fill("black");
-	textAlign(LEFT);
-	textSize(32);
-	text("Score: " 60, 50);
-}
+
 	
 class Hero{
 	constructor(){
-		this.x = x;
-		this.y = y;
+		this.x = 20;
+		this.y = 40;
 		this.width = 10;
 		this.height = 20;
 		this.velocityx = 0;
@@ -73,24 +73,22 @@ class Hero{
 	}
 	move(){
 		if(bob.contains(this.x, this.y) == false){
-			this.y = y++;
+			this.y++;
 		}
-		if(KeyIsDown(LEFT_ARROW)){
+		if(keyIsDown(LEFT_ARROW)){
 			this.x -= 5;
 		}
-		if(KeyIsDown(RIGHT_ARROW)){
+		if(keyIsDown(RIGHT_ARROW)){
 			this.x += 5;
 		}
-		if(KeyIsDown(UP_ARROW)){
+		/*if(keyIsDown(UP_ARROW)){
 			this.y -= 5;
 		}
-		if(KeyIsDown(DOWN_ARROW)){
+		if(keyIsDown(DOWN_ARROW)){
 			this.y += 5;
 		}
+		*/
 	}
-	
-
-
 	show(){
 		stroke("blue");
 		strokeWeight(4);
